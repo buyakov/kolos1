7d2b5bf957d97bd54b53631697f41e3f0d9aa5ab
obmenok@gmail.com <obmenok@gmail.com>
2024-12-01 22:17:32 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -30,6 +30,7 @@ class Form1(Form1Template):
       num = int(self.drop_down_1.selected_value)
 
       if self.status == 'Членский и целевой взнос':
+        self.image_1.visible = True
         # получаем площадь участка из таблицы
         area_value = [r['area_value'] for r in app_tables.table_2.search(number=num)][0]
         # собираем ссылку для генерации QR кода
@@ -38,8 +39,10 @@ class Form1(Form1Template):
         self.label_2.text = 'Участок № ' + str(num) + '\nЧленский взнос - ' + str(area_value) + ' ₽' + '\nЦелевой взнос - ' + str(1000) + ' ₽' + '\nСумма к оплате - ' + str(area_value+1000) + ' ₽'
         # передаем изображение QR кода на страницу
         self.image_1.source = link
+        self.image_2.visible = False
 
       elif self.status == 'Электроэнергия':
+        self.image_1.visible = True
         # расчитываем потребление электроэнергии
         consumption = self.t2 - self.t1
         if (self.text_box_1.text is not None) and (self.text_box_2.text is not None) and (consumption >= 0):
@@ -49,21 +52,29 @@ class Form1(Form1Template):
           self.label_2.text = 'Участок № ' + str(num) + '\nПотребление, кВт - ' + str(consumption) + '\nСумма к оплате - ' + str(consumption*4.5) + ' ₽'
           # передаем изображение QR кода на страницу
           self.image_1.source = link
+          self.image_2.visible = False
         else:
           self.label_2.text = 'Корректно введите показания'
           self.drop_down_1.selected_value = None
           self.text_box_1.text = None
           self.text_box_2.text = None
       elif self.status == 'Прошлые периоды':
-        # собираем ссылку для генерации QR кода
-        link = anvil.http.url_decode('https://www.bcgen.com/demo/IDAutomationStreamingQRCode.aspx?ECL=L&D=ST00012|Name=НЕКОММЕРЧЕСКОЕ САДОВОДЧЕСКОЕ ТОВАРИЩЕСТВО ""КОЛОС-1""|PersonalAcc=40703810400130000655|BankName=АО КБ ""ХЛЫНОВ"", Г.КИРОВ|BIC=043304711|CorrespAcc=30101810100000000711|PayeeINN=4346026874|KPP=434501001|Purpose=ПРОШЛЫЕ ПЕРИОДЫ, УЧАСТОК №' + str(num) + '|Sum=' + str(self.payment*100) + '&MODE=B&PT=T&X=0.1&O=0&LM=0.2&V=0')
-        # передаем информацию о платеже на страницу
-        self.label_2.text = 'Участок № ' + str(num) + '\nСумма к оплате - ' + str(self.payment) + ' ₽'
-        # передаем изображение QR кода на страницу
-        self.image_1.source = link
-
+        self.image_1.visible = True
+        if self.text_box_3.text is not None:
+          # собираем ссылку для генерации QR кода
+          link = anvil.http.url_decode('https://www.bcgen.com/demo/IDAutomationStreamingQRCode.aspx?ECL=L&D=ST00012|Name=НЕКОММЕРЧЕСКОЕ САДОВОДЧЕСКОЕ ТОВАРИЩЕСТВО ""КОЛОС-1""|PersonalAcc=40703810400130000655|BankName=АО КБ ""ХЛЫНОВ"", Г.КИРОВ|BIC=043304711|CorrespAcc=30101810100000000711|PayeeINN=4346026874|KPP=434501001|Purpose=ПРОШЛЫЕ ПЕРИОДЫ, УЧАСТОК №' + str(num) + '|Sum=' + str(self.payment*100) + '&MODE=B&PT=T&X=0.1&O=0&LM=0.2&V=0')
+          # передаем информацию о платеже на страницу
+          self.label_2.text = 'Участок № ' + str(num) + '\nСумма к оплате - ' + str(self.payment) + ' ₽'
+          # передаем изображение QR кода на страницу
+          self.image_1.source = link
+          self.image_2.visible = False
+        else:
+          self.label_2.text = 'Введите сумму'
+          self.drop_down_1.selected_value = None
+          self.text_box_3.text = None
       else:
         #собираем данные по участку из таблицы
+        self.image_2.visible = True
         cn = [r['cn'] for r in app_tables.table_2.search(number=num)][0]
         area_value = [r['area_value'] for r in app_tables.table_2.search(number=num)][0]
         address = [r['address'] for r in app_tables.table_2.search(number=num)][0]
@@ -86,7 +97,7 @@ class Form1(Form1Template):
 
         #передаеем данные об участке на страницу
         self.label_2.text = 'Участок № ' + str(num) + '\n\nИнформация из Росреестра:\n' + 'Кадастровый номер - ' + str(cn) + ',\nКадастровая стоимость - ' "{:.2f}".format(cad_cost) + ' ₽\n' + 'Дата внесения сведений о кадастровой стоимости в ГКН - ' + str(cc_date_entering) + '\nАдрес - ' + str(address) + '\nТип использования - ' + str(util_by_doc) + '\nПлощадь - ' + str(area_value) + ' м²'
-        #self.image_1.visible = False
+        self.image_1.visible = False
         self.image_2.source = [r['plan'] for r in app_tables.table_2.search(number=num)][0]
         
         #запрос на сервер для добавления данных в таблицу
@@ -146,6 +157,7 @@ class Form1(Form1Template):
       self.label_1.visible = True
     #устанавливаем видимость полей для статуса Схема участков
     if self.status == 'Схема участков':
+      self.image_1.visible = True
       self.text_box_1.visible = False
       self.text_box_2.visible = False
       self.text_box_3.visible = False
@@ -153,6 +165,7 @@ class Form1(Form1Template):
       self.label_1.visible = False
       link = 'https://i.postimg.cc/5bs26tpJ/Kolos-img.png'
       self.image_1.source = link
+      self.image_2.visible = False
 
     pass
 
