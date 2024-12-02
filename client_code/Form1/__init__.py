b604ba69c1232576cd197423685d51f146ee044c
obmenok@gmail.com <obmenok@gmail.com>
2024-12-02 18:34:26 +0000

    Edited ServerModule1, standard-page.html and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,23 +5,23 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import anvil.http
-import anvil.js
+#import anvil.js
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     self.text_box_1.visible = False
     self.text_box_2.visible = False
     self.text_box_3.visible = False
+    self.image_2.visible = False
     self.rich_text_1.visible= False
     self.status = 'Членский и целевой взнос'
     self.t1 = 0
     self.t2 = 0
 
     # Any code you write here will run before the form opens.
-  
+
   def drop_down_1_change(self, **event_args):
     """This method is called when an item is selected"""
 
@@ -31,7 +31,6 @@ class Form1(Form1Template):
       num = int(self.drop_down_1.selected_value)
 
       if self.status == 'Членский и целевой взнос':
-        self.image_1.visible = True
         # получаем площадь участка из таблицы
         area_value = [r['area_value'] for r in app_tables.table_2.search(number=num)][0]
         # собираем ссылку для генерации QR кода
@@ -40,10 +39,8 @@ class Form1(Form1Template):
         self.label_2.text = 'Участок № ' + str(num) + '\nЧленский взнос - ' + str(area_value) + ' ₽' + '\nЦелевой взнос - ' + str(1000) + ' ₽' + '\nСумма к оплате - ' + str(area_value+1000) + ' ₽'
         # передаем изображение QR кода на страницу
         self.image_1.source = link
-        self.image_2.visible = False
 
       elif self.status == 'Электроэнергия':
-        self.image_1.visible = True
         # расчитываем потребление электроэнергии
         consumption = self.t2 - self.t1
         if (self.text_box_1.text is not None) and (self.text_box_2.text is not None) and (consumption >= 0):
@@ -53,7 +50,6 @@ class Form1(Form1Template):
           self.label_2.text = 'Участок № ' + str(num) + '\nПотребление, кВт - ' + str(consumption) + '\nСумма к оплате - ' + str(consumption*4.5) + ' ₽'
           # передаем изображение QR кода на страницу
           self.image_1.source = link
-          self.image_2.visible = False
         else:
           self.label_2.text = 'Корректно введите показания'
           self.drop_down_1.selected_value = None
@@ -61,7 +57,6 @@ class Form1(Form1Template):
           self.text_box_2.text = None
           self.image_1.source = None
       elif self.status == 'Прошлые периоды':
-        self.image_1.visible = True
         if self.text_box_3.text is not None:
           # собираем ссылку для генерации QR кода
           link = anvil.http.url_decode('https://www.bcgen.com/demo/IDAutomationStreamingQRCode.aspx?ECL=L&D=ST00012|Name=НЕКОММЕРЧЕСКОЕ САДОВОДЧЕСКОЕ ТОВАРИЩЕСТВО ""КОЛОС-1""|PersonalAcc=40703810400130000655|BankName=АО КБ ""ХЛЫНОВ"", Г.КИРОВ|BIC=043304711|CorrespAcc=30101810100000000711|PayeeINN=4346026874|KPP=434501001|Purpose=ПРОШЛЫЕ ПЕРИОДЫ, УЧАСТОК №' + str(num) + '|Sum=' + str(self.payment*100) + '&MODE=B&PT=T&X=0.1&O=0&LM=0.2&V=0')
@@ -69,7 +64,6 @@ class Form1(Form1Template):
           self.label_2.text = 'Участок № ' + str(num) + '\nСумма к оплате - ' + str(self.payment) + ' ₽'
           # передаем изображение QR кода на страницу
           self.image_1.source = link
-          self.image_2.visible = False
         else:
           self.label_2.text = 'Введите сумму'
           self.drop_down_1.selected_value = None
@@ -77,7 +71,6 @@ class Form1(Form1Template):
           self.image_1.source = None
       else:
         #собираем данные по участку из таблицы
-        self.image_2.visible = True
         cn = [r['cn'] for r in app_tables.table_2.search(number=num)][0]
         area_value = [r['area_value'] for r in app_tables.table_2.search(number=num)][0]
         address = [r['address'] for r in app_tables.table_2.search(number=num)][0]
@@ -89,7 +82,6 @@ class Form1(Form1Template):
         #linkapi = 'https://pkk.rosreestr.ru/api/features/1/43:40:32706:' + str(num)
         # делаем запрос на сайт Росреестра, работает через раз
         #response = anvil.http.request(linkapi, json=True)
-    
         # из json файла берем нужные данные
         #cn = response['feature']['attrs']['cn'] #кадастровый номер
         #cad_cost = response['feature']['attrs']['cad_cost'] #кадастровая стоимость
@@ -100,84 +92,95 @@ class Form1(Form1Template):
 
         #передаеем данные об участке на страницу
         self.label_2.text = 'Участок № ' + str(num) + '\n\nИнформация из Росреестра:\n' + 'Кадастровый номер - ' + str(cn) + ',\nКадастровая стоимость - ' "{:.2f}".format(cad_cost) + ' ₽\n' + 'Дата внесения сведений о кадастровой стоимости в ГКН - ' + str(cc_date_entering) + '\nАдрес - ' + str(address) + '\nТип использования - ' + str(util_by_doc) + '\nПлощадь - ' + str(area_value) + ' м²'
-        self.image_1.visible = False
+        #self.image_1.visible = False
         self.image_2.source = [r['plan'] for r in app_tables.table_2.search(number=num)][0]
         
         #запрос на сервер для добавления данных в таблицу
         #anvil.server.call('add_row', cn, cad_cost, address, util_by_doc, cc_date_entering, area_value)
-
     else:
-      self.image_1.source = None
-      self.label_2.text = None
       self.text_box_1.text = None
       self.text_box_2.text = None
       self.text_box_3.text = None
+      self.label_2.text = None
       self.image_1.source = None
       self.image_2.source = None
-
+      self.rich_text_1.content = None
     pass
 
   def drop_down_2_change(self, **event_args):
     """This method is called when an item is selected"""
     #сбрасываем значения полей при смене статуса
-    self.label_2.text = None
     self.text_box_1.text = None
     self.text_box_2.text = None
     self.text_box_3.text = None
+    self.label_2.text = None
     self.image_1.source = None
     self.image_2.source = None
+    self.drop_down_1.selected_value = None
     #сохраняем в переменную значение статуса
     self.status = self.drop_down_2.selected_value
-    #сбрасываем поле выбора номера участка при смене статуса
-    self.drop_down_1.selected_value = None
     #устанавливаем видимость полей для статуса Членский и целевой взнос
     if self.status == 'Членский и целевой взнос':
       self.text_box_1.visible = False
       self.text_box_2.visible = False
       self.text_box_3.visible = False
-      self.drop_down_1.visible = True
       self.label_1.visible = True
       self.label_2.visible = True
+      self.drop_down_1.visible = True
+      self.drop_down_2.visible = True
+      self.image_1.visible = True
+      self.image_2.visible = False
       self.rich_text_1.visible= False
     #устанавливаем видимость полей для статуса Электроэнергия
     if self.status == 'Электроэнергия':
       self.text_box_1.visible = True
       self.text_box_2.visible = True
       self.text_box_3.visible = False
-      self.drop_down_1.visible = True
       self.label_1.visible = True
       self.label_2.visible = True
+      self.drop_down_1.visible = True
+      self.drop_down_2.visible = True
+      self.image_1.visible = True
+      self.image_2.visible = False
       self.rich_text_1.visible= False
     #устанавливаем видимость полей для статуса Прошлые периоды
     if self.status == 'Прошлые периоды':
       self.text_box_1.visible = False
       self.text_box_2.visible = False
       self.text_box_3.visible = True
-      self.drop_down_1.visible = True
       self.label_1.visible = True
       self.label_2.visible = True
+      self.drop_down_1.visible = True
+      self.drop_down_2.visible = True
+      self.image_1.visible = True
+      self.image_2.visible = False
       self.rich_text_1.visible= False
     #устанавливаем видимость полей для статуса Информация об участке
     if self.status == 'Информация об участке':
       self.text_box_1.visible = False
       self.text_box_2.visible = False
       self.text_box_3.visible = False
-      self.drop_down_1.visible = True
       self.label_1.visible = True
       self.label_2.visible = True
+      self.drop_down_1.visible = True
+      self.drop_down_2.visible = True
+      self.image_1.visible = False
+      self.image_2.visible = True
       self.rich_text_1.visible= False
     #устанавливаем видимость полей для статуса Схема участков
     if self.status == 'Схема участков':
-      self.image_1.visible = True
       self.text_box_1.visible = False
       self.text_box_2.visible = False
       self.text_box_3.visible = False
-      self.drop_down_1.visible = False
       self.label_1.visible = False
+      self.label_2.visible = False
+      self.drop_down_1.visible = False
+      self.drop_down_2.visible = True
+      self.image_1.visible = True
+      self.image_2.visible = False
       self.rich_text_1.visible= False
       link = 'https://i.postimg.cc/5bs26tpJ/Kolos-img.png'
       self.image_1.source = link
-      self.image_2.visible = False
     #устанавливаем видимость полей для статуса Контакты и реквизиты
     if self.status == 'Контакты и реквизиты':
       self.text_box_1.visible = False
@@ -186,22 +189,27 @@ class Form1(Form1Template):
       self.label_1.visible = False
       self.label_2.visible = False
       self.drop_down_1.visible = False
-      self.label_1.visible = False
+      self.drop_down_2.visible = True
       self.image_1.visible = False
       self.image_2.visible = False
       self.rich_text_1.visible = True
     pass
 
+  #запись переменной t1 из поля Предыдущие показания
   def text_box_1_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
+    #если значение поля не пустое
     if self.text_box_1.text is not None:
+      #если поле не целое или отрицательное
       if not isinstance(self.text_box_1.text, int) or self.text_box_1.text < 0:
+        #то очищаем поле
         self.text_box_1.text = None
+      #в противном случае присваеваем значение поля в перемнную
       else:
           self.t1 = int(self.text_box_1.text)
-
     pass
 
+  #запись переменной t2 из поля Текущие показания
   def text_box_2_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     if self.text_box_2.text is not None:
@@ -211,6 +219,7 @@ class Form1(Form1Template):
           self.t2 = int(self.text_box_2.text)
     pass
 
+  #запись переменной payment из поля Сумма
   def text_box_3_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     if self.text_box_3.text is not None:
@@ -220,10 +229,10 @@ class Form1(Form1Template):
           self.payment = int(self.text_box_3.text)
     pass
 
-    def __init__(self, **properties):
-      self.init_components(**properties)
+#    def __init__(self, **properties):
+#      self.init_components(**properties)
     
       # Call the JavaScript function and pass the ID of the component
-      anvil.js.call_js('setNumericKeyboard', self.text_box_1)
-      anvil.js.call_js('setNumericKeyboard', self.text_box_2)
-      anvil.js.call_js('setNumericKeyboard', self.text_box_3)
+#      anvil.js.call_js('setNumericKeyboard', self.text_box_1)
+#      anvil.js.call_js('setNumericKeyboard', self.text_box_2)
+#      anvil.js.call_js('setNumericKeyboard', self.text_box_3)
