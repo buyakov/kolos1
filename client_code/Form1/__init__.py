b89d6b88783d55e6b71da321b9d3dd118579c879
obmenok@gmail.com <obmenok@gmail.com>
2025-04-03 06:57:28 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -27,8 +27,8 @@ class Form1(Form1Template):
     self.t2 = 0
 
     # Create an iframe element and set the src
-    iframe_yandex = jQuery("<iframe width='100%' height='400' frameborder='0'>").attr("src","https://yandex.ru/map-widget/v1/?um=constructor%3A8c78e07a751d5a9e657860e397d28e92a92ff8ebc0fb8a8ba7be72ea7417b931&amp;source=constructor")
-    iframe_google = jQuery("<iframe width='100%' height='400' frameborder='0'>").attr("src","https://www.google.com/maps/d/embed?mid=1Va23cJVcImyNFZfrASGqnYF-aqVdHzQ&ehbc=2E312F&noprof=1")
+    iframe_yandex = jQuery("<iframe width='100%' height='500' frameborder='0'>").attr("src","https://yandex.ru/map-widget/v1/?um=constructor%3A8c78e07a751d5a9e657860e397d28e92a92ff8ebc0fb8a8ba7be72ea7417b931&amp;source=constructor")
+    iframe_google = jQuery("<iframe width='100%' height='500' frameborder='0'>").attr("src","https://www.google.com/maps/d/embed?mid=1Va23cJVcImyNFZfrASGqnYF-aqVdHzQ&ehbc=2E312F&noprof=1")
     # Append the iframe to a container in our form
     iframe_yandex.appendTo(get_dom_node(self.linear_panel_1))
     iframe_google.appendTo(get_dom_node(self.linear_panel_2))
